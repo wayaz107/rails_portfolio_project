2746831150a85e488533ac0615705713274154e8
Wardayaz <wayaz107@gmail.com>
2020-12-17 00:59:55 -0600

    added login details in sessions controller and made a new sessions view file

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,20 @@ class SessionsController < ApplicationController
     end 
 
     def create
+        @user = User.find_by(email: params[:user][:email])
+        if params[:user][:email] == "" || params[:user][:password] == ""
+            redirect_to login_path
+        elsif @user && @user.try(:authenticate, params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            redirect_to login_path
+        end 
     end 
 
     def destroy
+        session.destroy
+        redirect_to root_path
     end 
-    
+
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   get '/' => 'welcome#home'
 
   get '/signup' => 'users#new'
-  post '/signup' => 'users#create
+  post '/signup' => 'users#create' 
 
-  get '/login' => 'sessions#new'
+  get  '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
 
