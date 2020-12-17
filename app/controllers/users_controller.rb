de624e898ea79ed039ded2c7c382b20311bcef59
Wardayaz <wayaz107@gmail.com>
2020-12-17 01:49:35 -0600

    created signup and login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,27 @@
 class UsersController < ApplicationController
     
+    def show
+    end 
+    
+    
     def new
+        @user = User.new
     end 
 
     def create
-    end 
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            render :new
+          end
+        end
 
+        private
+        def user_params
+          params.require(:user).permit(:email, :name, :password)
+        end
+      
     
 end
