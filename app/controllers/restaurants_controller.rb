67fd630a3951507834add9b908effcd3234035df
Wardayaz <wayaz107@gmail.com>
2020-12-18 00:55:11 -0600

    added helper methods and methods to retaurant controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,22 @@
 class RestaurantsController < ApplicationController
+   
+    def new 
+    end 
+
+    def create
+    end 
+
+    def index
+    end 
+
+    def show
+    end 
+
+    def edit
+    end 
+
+    def update
+    end 
+
+
 end
