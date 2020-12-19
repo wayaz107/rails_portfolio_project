7e76eeb93d0252598eecfb323b10555a5f768999
Wardayaz <wayaz107@gmail.com>
2020-12-18 23:50:19 -0600

    added cuisine index method and index view page

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -1,5 +1,12 @@
 class CuisinesController < ApplicationController
-    def show
-       
-      end
+   
+  def index
+    @cuisines = Cuisine.all
+  end 
+  
+  
+  def show
+    @cuisine = Cuisine.find(params[:id]) 
+  end
+
 end
