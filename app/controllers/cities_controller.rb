9f3ad4a2de0f490b7aed69af56dcc770b0601049
Wardayaz <wayaz107@gmail.com>
2020-12-19 00:00:03 -0600

    created cities index and show methods and views

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,5 +1,11 @@
 class CitiesController < ApplicationController
-    def show
-        
-      end
+    
+  def index
+    @cities = City.all
+  end 
+  
+  def show
+    @city = City.find(params[:id])
+  end
+
 end
