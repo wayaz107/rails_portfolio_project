4ae772ebc586aa13bc859c17c01152e18d9d3d4a
Wardayaz <wayaz107@gmail.com>
2020-12-19 04:01:04 -0600

    added reviews index controller and index view page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,18 @@
 class ReviewsController < ApplicationController
+
+    def index
+        @user = User.find_by(id: params[:user_id])
+        @reviews = @user.reviews.all
+    end	  
+
+    def new
+    end 
+
+    def show
+    end 
+
+    def edit
+    end 
+
+
 end
